f5fc3c3649e6b8f1b54c383f172e057bb99e8b25
Mischosch <michael@wesrc.com>
2021-08-02 02:00:28 +0200

    add rbac config setup

---
+++ config/rbac.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+use CooarchiApp\Handler;
+use CooarchiEntities;
+
+return [
+    'roles' => [
+        CooarchiEntities\User::ROLE_ADMINISTRATA => [],
+        CooarchiEntities\User::ROLE_KOLLEKTIVISTA => [CooarchiEntities\User::ROLE_ADMINISTRATA],
+    ],
+    'permissions' => [
+        CooarchiEntities\User::ROLE_KOLLEKTIVISTA => [
+            Handler\GetDataHandler::ROUTE,
+            Handler\SaveHandler::ROUTE,
+            Handler\UploadHandler::ROUTE,
+        ],
+        CooarchiEntities\User::ROLE_ADMINISTRATA => [
+            Handler\PingHandler::ROUTE,
+        ],
+    ],
+];
