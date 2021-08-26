15e5b0a0066531e1698c6700eda6ac30b7d39d6a
Mischosch <michael@wesrc.com>
2021-08-27 01:12:44 +0200

    add logic to handle default travella Accounts

--- src/App/src/Middleware/AuthFactory.php
+++ src/App/src/Middleware/AuthFactory.php
@@ -6,15 +6,23 @@ namespace CooarchiApp\Middleware;
 
 use Interop\Container\ContainerInterface;
 use Laminas\Authentication\AuthenticationService;
+use LogicException;
 use Mezzio\Helper\UrlHelper;
 
 final class AuthFactory
 {
     public function __invoke(ContainerInterface $container) : AuthMiddleware
     {
+        $cooarchiConfig = $container->get('config')['cooarchi'] ?? [];
+        if ($cooarchiConfig === []) {
+            throw new LogicException('cooarchi config is missing');
+        }
+
         return new AuthMiddleware(
             $container->get(AuthenticationService::class),
-            $container->get(UrlHelper::class)
+            $container->get(UrlHelper::class),
+            $cooarchiConfig['isPublicReadable'] ?? false,
+            $cooarchiConfig['isPublicWriteable'] ?? false
         );
     }
 }
--- src/App/src/Middleware/AuthMiddleware.php
+++ src/App/src/Middleware/AuthMiddleware.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace CooarchiApp\Middleware;
 
+use CooarchiApp\Authentication;
 use CooarchiApp\Handler\AuthStatusHandler;
 use CooarchiApp\Handler\HomeHandler;
 use CooarchiApp\Handler\RegistrationHandler;
+use CooarchiEntities\User;
+use Laminas\Crypt\Password\Bcrypt;
 use Mezzio\Router\RouteResult;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -24,15 +27,29 @@ final class AuthMiddleware implements MiddlewareInterface
     private $authenticationService;
 
     /**
+     * @var bool
+     */
+    private $isPublicReadable;
+
+    /**
+     * @var bool
+     */
+    private $isPublicWriteable;
+
+    /**
      * @var UrlHelper
      */
     private $urlHelper;
 
     public function __construct(
         AuthenticationService $authenticationService,
-        UrlHelper $urlHelper
+        UrlHelper $urlHelper,
+        bool $isPublicReadable,
+        bool $isPublicWriteable
     ) {
         $this->authenticationService = $authenticationService;
+        $this->isPublicReadable = $isPublicReadable;
+        $this->isPublicWriteable = $isPublicWriteable;
         $this->urlHelper = $urlHelper;
     }
 
@@ -41,11 +58,23 @@ final class AuthMiddleware implements MiddlewareInterface
         $route = $request->getAttribute(RouteResult::class);
         $routeName = $route->getMatchedRoute()->getName();
 
+        $identity = $this->authenticationService->getIdentity();
+
+        // If identity exist, we can skip further logic and just sent identity against next middleware
+        if ($identity !== null) {
+            return $delegate->handle($request->withAttribute('identity', $identity));
+        }
+
         if ($routeName !== HomeHandler::ROUTE_NAME &&
             $routeName !== AuthStatusHandler::ROUTE_NAME &&
             $routeName !== RegistrationHandler::ROUTE_NAME &&
             $this->authenticationService->hasIdentity() === false
         ) {
+            if ($this->isPublicReadable === true || $this->isPublicWriteable === true) {
+                $travellaIdentity = new User('travella', 'foobar', User::ROLE_TRAVELLA);
+                return $delegate->handle($request->withAttribute('identity', $travellaIdentity));
+            }
+
             return new RedirectResponse($this->urlHelper->generate('login'));
         }
 
