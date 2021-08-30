51edc581e1dc408d73bc851e7083d793f5c4fbbc
Mischosch <michael@wesrc.com>
2021-08-30 03:14:11 +0200

    inject cooArchi config to show welcomeText

--- src/App/src/Handler/HomeHandler.php
+++ src/App/src/Handler/HomeHandler.php
@@ -9,6 +9,7 @@ use Mezzio\Template\TemplateRendererInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Flash\Messages;
 
 class HomeHandler implements RequestHandlerInterface
 {
@@ -16,27 +17,39 @@ class HomeHandler implements RequestHandlerInterface
     public const ROUTE_NAME = 'home';
 
     /**
+     * @var array
+     */
+    private $cooArchiConfig;
+
+    /**
      * @var TemplateRendererInterface
      */
     private $template;
 
     public function __construct(
-        TemplateRendererInterface $template
+        TemplateRendererInterface $template,
+        array $cooArchiConfig
     ) {
+        $this->cooArchiConfig = $cooArchiConfig;
         $this->template  = $template;
     }
 
     public function handle(ServerRequestInterface $request) : ResponseInterface
     {
+        /** @var Messages $flashMessages */
+        $flashMessages = $request->getAttribute('flash');
+
         $data = [];
+        $data['welcomeText'] = $this->cooArchiConfig['welcomeText'];
 
-        $user = $request->getAttribute('identity', false);
-
-        $this->template->addDefaultParam(
-            TemplateRendererInterface::TEMPLATE_ALL,
-            'identity',
-            $user
-        );
+        $messagesSuccess = $flashMessages->getMessage('success');
+        if (is_array($messagesSuccess) === true) {
+            $data['success'] = $messagesSuccess[0];
+        }
+        $messagesError = $flashMessages->getMessage('error');
+        if (is_array($messagesError) === true) {
+            $data['error'] = $messagesError[0];
+        }
 
         return new HtmlResponse($this->template->render('app::home', $data));
     }
--- src/App/src/Handler/HomeHandlerFactory.php
+++ src/App/src/Handler/HomeHandlerFactory.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace CooarchiApp\Handler;
 
+use LogicException;
 use Mezzio\Template\TemplateRendererInterface;
 use Psr\Container\ContainerInterface;
 
@@ -10,8 +11,14 @@ final class HomeHandlerFactory
 {
     public function __invoke(ContainerInterface $container) : HomeHandler
     {
+        $cooarchiConfig = $container->get('config')['cooarchi'] ?? [];
+        if ($cooarchiConfig === []) {
+            throw new LogicException('cooarchi config is missing');
+        }
+
         return new HomeHandler(
-            $container->get(TemplateRendererInterface::class)
+            $container->get(TemplateRendererInterface::class),
+            $cooarchiConfig
         );
     }
 }
