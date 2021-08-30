b986fc63bada856f34a74d6f4d090898fb891b90
Mischosch <michael@wesrc.com>
2021-08-30 03:09:45 +0200

    add byId method to FindUser query

--- src/Queries/src/FindUser.php
+++ src/Queries/src/FindUser.php
@@ -8,6 +8,16 @@ use CooarchiEntities;
 
 final class FindUser extends Base
 {
+    public function byId(string $userId) : ?CooarchiEntities\User
+    {
+        $query = $this->entityManager->createQuery("
+            SELECT user FROM CooarchiEntities\User user WHERE user.id = :userId
+        ");
+        $query->setParameter('userId', $userId);
+
+        return $query->getOneOrNullResult();
+    }
+
     public function byName(string $name) : ?CooarchiEntities\User
     {
         $query = $this->entityManager->createQuery("
